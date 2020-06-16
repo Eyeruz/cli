e6837d8d6d27178a981aa75b431c3be3aa35949e
Eyeruz <3yerus@gmail.com>
2020-06-16 12:05:45 -0400

    purrr

---
+++ lib/cats.rb
@@ -0,0 +1,6 @@
+require_relative "./cli_project/version"
+require_relative "./bin/cli"
+
+module CliProject
+
+end
---
+++ lib/cli.rb
@@ -0,0 +1,12 @@
+
+
+class Cats::CLI
+
+
+    def call 
+
+        puts "hey you're purrrfect"
+
+    end
+
+end
